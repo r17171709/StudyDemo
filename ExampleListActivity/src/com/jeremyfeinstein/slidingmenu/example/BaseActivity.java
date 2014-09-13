3a4eb6a73b1a6cbed842383684c01851ac869767
r17171709 <Yuren2@hotmail.com>
2014-09-13 17:43:39 +0800

    利用SlidingMenu仿AndroidResideMenu最新滑动特效，主要是在原先的菜单布局动画基础上模仿，扩展出主页面的动画效果

---
+++ ExampleListActivity/src/com/jeremyfeinstein/slidingmenu/example/BaseActivity.java
@@ -0,0 +1,87 @@
+package com.jeremyfeinstein.slidingmenu.example;
+
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
+
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu.CanvasTransformer;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+
+public class BaseActivity extends SlidingFragmentActivity {
+
+	private int mTitleRes;
+	protected ListFragment mFrag;
+
+	public BaseActivity(int titleRes) {
+		mTitleRes = titleRes;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setTitle(mTitleRes);
+
+		// set the Behind View
+		setBehindContentView(R.layout.menu_frame);
+		if (savedInstanceState == null) {
+			FragmentTransaction t = this.getSupportFragmentManager().beginTransaction();
+			mFrag = new SampleListFragment();
+			t.replace(R.id.menu_frame, mFrag);
+			t.commit();
+		} else {
+			mFrag = (ListFragment)this.getSupportFragmentManager().findFragmentById(R.id.menu_frame);
+		}
+
+		// customize the SlidingMenu
+		SlidingMenu sm = getSlidingMenu();
+		sm.setShadowWidthRes(R.dimen.shadow_width);
+		sm.setShadowDrawable(R.drawable.shadow);
+		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		sm.setFadeDegree(0.35f);
+		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		sm.setBehindCanvasTransformer(new CanvasTransformer() {
+			
+			@Override
+			public void transformCanvas(Canvas canvas, float percentOpen) {
+				// TODO Auto-generated method stub
+				float scale=(float)(percentOpen*0.25+0.75);
+				canvas.scale(scale, scale, -canvas.getWidth(), canvas.getHeight()/2);
+			}
+		});
+		sm.setAboveCanvasTransformer(new CanvasTransformer() {
+			
+			@Override
+			public void transformCanvas(Canvas canvas, float percentOpen) {
+				// TODO Auto-generated method stub
+				float scale=(float)(1-percentOpen*0.25);
+				canvas.scale(scale, scale, 0, canvas.getHeight()/2);
+			}
+		});
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			toggle();
+			return true;
+		case R.id.github:
+			Util.goToGitHub(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
