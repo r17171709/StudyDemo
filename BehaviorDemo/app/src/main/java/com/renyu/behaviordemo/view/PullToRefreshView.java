60e8ac26c2d7a8fdc734234f3ecd249efe1f0136
renyu <Yuren2@hotmail.com>
2016-12-15 17:09:49 +0800

    CoordinatorLayout中Behavior的使用

--- BehaviorDemo/app/src/main/java/com/renyu/behaviordemo/view/PullToRefreshView.java
+++ BehaviorDemo/app/src/main/java/com/renyu/behaviordemo/view/PullToRefreshView.java
@@ -23,8 +23,8 @@ public class PullToRefreshView extends LinearLayout implements NestedScrollingPa
 
     Context context;
 
+    //由于onStopNestedScroll每次手势事件都会被优先触发，会造成事件混乱，所以稍微处理一下
     boolean canScrollExtra=false;
-
     //每次移动的偏移量
     int deltaY=0;
     //是否屏蔽下拉事件
@@ -44,6 +44,7 @@ public class PullToRefreshView extends LinearLayout implements NestedScrollingPa
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
 
+        //初始化head、foot、本体
         getChildAt(0).layout(0, dp2Px(context, -200), getMeasuredWidth(), 0);
         getChildAt(1).layout(0, 0, getMeasuredWidth(), getMeasuredHeight());
         getChildAt(2).layout(0, getMeasuredHeight(), getMeasuredWidth(), getMeasuredHeight()+dp2Px(context, 200));
@@ -56,12 +57,16 @@ public class PullToRefreshView extends LinearLayout implements NestedScrollingPa
 
     @Override
     public void onNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        //下拉刷新
         if (dyUnconsumed<0) {
+            //滑动范围
             int max=dp2Px(context, 200);
             int min=0;
             scrollDown(this, dyUnconsumed, min, max);
         }
+        //上拉加载更多
         else if (dyUnconsumed>0) {
+            //滑动范围
             int min=-dp2Px(context, 200);
             int max=0;
             scrollUp(this, dyUnconsumed, min, max);
@@ -70,11 +75,13 @@ public class PullToRefreshView extends LinearLayout implements NestedScrollingPa
 
     @Override
     public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
+        //下拉刷新
         if (getChildAt(1).getTop()>0) {
             int max=dp2Px(context, 200);
-            int min=1;
+            int min=0;
             consumed[1]=scrollDown(this, dy, min, max);
         }
+        //上拉加载更多
         else if (getChildAt(1).getTop()<0) {
             int min=-dp2Px(context, 200);
             int max=0;
@@ -139,7 +146,8 @@ public class PullToRefreshView extends LinearLayout implements NestedScrollingPa
                     super.onAnimationEnd(animation);
                     deltaY=0;
                     if (isIntercept) {
-                        changeText();
+                        ((ImageView) ((LinearLayout) getChildAt(0)).getChildAt(0)).setImageResource(R.mipmap.ic_launcher);
+                        ((ImageView) ((LinearLayout) getChildAt(2)).getChildAt(0)).setImageResource(R.mipmap.ic_launcher);
                         new Handler().postDelayed(new Runnable() {
                             @Override
                             public void run() {
@@ -253,39 +261,33 @@ public class PullToRefreshView extends LinearLayout implements NestedScrollingPa
         //上拉加载更多
         if (getChildAt(1).getTop()<0) {
             //判断临界点上方还是下方
-            if (getChildAt(1).getTop()>-dp2Px(context, 200)/2) {
+            if (getChildAt(1).getTop()>(-dp2Px(context, 200)/2)) {
                 ((ImageView) ((LinearLayout) getChildAt(2)).getChildAt(0)).setImageResource(R.mipmap.ic_grade_level_1);
             }
-            else if (getChildAt(1).getTop()<-dp2Px(context, 200)/2) {
+            else if (getChildAt(1).getTop()<(-dp2Px(context, 200)/2)) {
                 ((ImageView) ((LinearLayout) getChildAt(2)).getChildAt(0)).setImageResource(R.mipmap.ic_grade_level_2);
-            }
-            else {
-                ((ImageView) ((LinearLayout) getChildAt(2)).getChildAt(0)).setImageResource(R.mipmap.ic_launcher);
             }
         }
         //下拉刷新
         else if (getChildAt(1).getTop()>0) {
             //判断临界点上方还是下方
-            if (getChildAt(1).getTop()>dp2Px(context, 200)/2) {
+            if (getChildAt(1).getTop()>(dp2Px(context, 200)/2)) {
                 ((ImageView) ((LinearLayout) getChildAt(0)).getChildAt(0)).setImageResource(R.mipmap.ic_grade_level_2);
             }
-            else if (getChildAt(1).getTop()<dp2Px(context, 200)/2) {
+            else if (getChildAt(1).getTop()<(dp2Px(context, 200)/2)) {
                 ((ImageView) ((LinearLayout) getChildAt(0)).getChildAt(0)).setImageResource(R.mipmap.ic_grade_level_1);
             }
-            else {
-                ((ImageView) ((LinearLayout) getChildAt(0)).getChildAt(0)).setImageResource(R.mipmap.ic_launcher);
-            }
         }
-    }
-
-    private int dp2Px(Context context, float dp) {
-        float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dp * scale + 0.5f);
     }
 
     //对资源进行设置的时候(比如setImageResource或者setText等)，会发生requestLayout
     @Override
     public void requestLayout() {
 
+    }
+
+    private int dp2Px(Context context, float dp) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
     }
 }
