c4c6cb3b8bd87eb3fbe36f5791f58001f4db014e
r17171709 <Yuren2@hotmail.com>
2018-04-25 08:08:52 +0800

    Rebound使用Demo

---
+++ tantan/app/src/main/java/com/example/tantan/ReboundActivity.kt
@@ -0,0 +1,47 @@
+package com.example.tantan
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.facebook.rebound.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * Created by Administrator on 2018/4/24.
+ */
+class ReboundActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        iv_main.setOnClickListener {
+            val springSystem = SpringSystem.create()
+            val scaleSpring = springSystem.createSpring()
+            scaleSpring.currentValue = 0.0
+            scaleSpring.springConfig = SpringConfig.fromOrigamiTensionAndFriction(40.0, 5.0)
+            scaleSpring.addListener(object : SpringListener {
+                override fun onSpringUpdate(spring: Spring?) {
+                    val mappedValue = SpringUtil.mapValueFromRangeToRange(spring?.currentValue!!, 0.0, 1.0, 0.5, 1.0).toFloat()
+
+                    println(mappedValue)
+                    iv_main.scaleX = mappedValue
+                    iv_main.scaleY = mappedValue
+                }
+
+                override fun onSpringEndStateChange(spring: Spring?) {
+
+                }
+
+                override fun onSpringAtRest(spring: Spring?) {
+                    scaleSpring.setAtRest()
+                }
+
+                override fun onSpringActivate(spring: Spring?) {
+
+                }
+            })
+
+            scaleSpring.endValue = 1.0
+        }
+    }
+}
