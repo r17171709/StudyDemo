c6e191b4b026030e75250ccb8df2b083cabf8d30
renyu <Yuren2@hotmail.com>
2020-11-19 08:51:46 +0800

    更新Kotlin协程使用范例

--- CoroutineDemo/app/src/main/java/com/renyu/coroutinedemo/MainActivity.kt
+++ CoroutineDemo/app/src/main/java/com/renyu/coroutinedemo/MainActivity.kt
@@ -96,7 +96,11 @@ class MainActivity() : AppCompatActivity(), CoroutineScope by MainScope() {
 
 //        vm.getHttpRequest2()
 
-//        vm.getHttpRequest3().observe(this, { t -> t })
+//        vm.getHttpRequest3().observe(this, { t ->
+//            run {
+//                Log.d("TAGTAGTAG", "${Looper.myLooper() == Looper.getMainLooper()}")
+//            }
+//        })
 
         val job2 = launch(start = CoroutineStart.LAZY) {
             Log.d("TAG", "launch coroutine : ${Thread.currentThread().name}")
