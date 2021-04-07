9eaa501d28fc0d84d527d89fe83a0db54bbaa80f
renyu <Yuren2@hotmail.com>
2021-04-07 14:22:34 +0800

    CordinatorLayout使用范例

---
+++ CoordinatorLaoyutDemo/app/src/main/java/com/renyu/coordinatorlaoyutdemo/MaterialButtonActivity.kt
@@ -0,0 +1,12 @@
+package com.renyu.coordinatorlaoyutdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MaterialButtonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_materialbutton)
+    }
+}
