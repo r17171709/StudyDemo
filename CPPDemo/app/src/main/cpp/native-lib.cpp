4abca77c56783409348dfe0bdb59b3b04e4e76b2
renyu <Yuren2@hotmail.com>
2021-06-28 11:03:20 +0800

    CPP Demo调整

--- CPPDemo/app/src/main/cpp/native-lib.cpp
+++ CPPDemo/app/src/main/cpp/native-lib.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include "MD5.h"
 
+/**
+ * 字符串拼接方法
+ * https://blog.csdn.net/cloverjf/article/details/78851988
+ */
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_house365_rent_RentAuth_stringFromJNI(
         JNIEnv *env,
@@ -27,29 +31,42 @@ Java_com_house365_rent_RentAuth_stringFromJNI(
 //    return env->NewStringUTF(md5Result.c_str());
 
 
-
-    const char *str1 = env->GetStringUTFChars(token, JNI_FALSE);
-    const char *str2 = env->GetStringUTFChars(auth_time, JNI_FALSE);
-    const char *str3 = env->GetStringUTFChars(auth_version, JNI_FALSE);
-    jstring vcode = env->NewStringUTF("AAAAB3NzaC1yc2EAAAADAQABAAABAQDNeYnErQ4Jf/h+OK+az2WXGFRnod2j6TGmzl8LsUnqcnta3ZgvF/R/VV");
-    const char *str4 = env->GetStringUTFChars(vcode, JNI_FALSE);
-
-    char *c = (char *) malloc(strlen(str1) + strlen(str2) + 1);
-    strcpy(c, str1);
-    strcat(c, str2);
-
-    char *c1 = (char *) malloc(strlen(str3) + strlen(str4) + 1);
-    strcpy(c1, str3);
-    strcat(c1, str4);
-
-    char *cResult = (char *) malloc(strlen(c) + strlen(c1) + 1);
-    strcpy(cResult, c);
-    strcat(cResult, c1);
-
+    jstring vcode = env->NewStringUTF(
+            "AAAAB3NzaC1yc2EAAAADAQABAAABAQDNeYnErQ4Jf/h+OK+az2WXGFRnod2j6TGmzl8LsUnqcnta3ZgvF/R/VV");
+    jclass String_clazz = env->FindClass("java/lang/String");
+    jmethodID concat_methodID = env->GetMethodID(String_clazz, "concat",
+                                                 "(Ljava/lang/String;)Ljava/lang/String;");
+    jobject str1 = env->CallObjectMethod(token, concat_methodID, auth_time);
+    jobject str2 = env->CallObjectMethod((jstring) str1, concat_methodID, auth_version);
+    jobject str3 = env->CallObjectMethod((jstring) str2, concat_methodID, vcode);
+    const char *cResult = env->GetStringUTFChars((jstring) str3, JNI_FALSE);
+    //释放内存
     env->DeleteLocalRef(vcode);
-    env->ReleaseStringUTFChars(token, str1);
-    env->ReleaseStringUTFChars(auth_time, str2);
-    env->ReleaseStringUTFChars(auth_version, str3);
+    env->ReleaseStringUTFChars((jstring) str3, cResult);
+
+
+//    const char *str1 = env->GetStringUTFChars(token, JNI_FALSE);
+//    const char *str2 = env->GetStringUTFChars(auth_time, JNI_FALSE);
+//    const char *str3 = env->GetStringUTFChars(auth_version, JNI_FALSE);
+//    jstring vcode = env->NewStringUTF("AAAAB3NzaC1yc2EAAAADAQABAAABAQDNeYnErQ4Jf/h+OK+az2WXGFRnod2j6TGmzl8LsUnqcnta3ZgvF/R/VV");
+//    const char *str4 = env->GetStringUTFChars(vcode, JNI_FALSE);
+//
+//    char *c = (char *) malloc(strlen(str1) + strlen(str2) + 1);
+//    strcpy(c, str1);
+//    strcat(c, str2);
+//
+//    char *c1 = (char *) malloc(strlen(str3) + strlen(str4) + 1);
+//    strcpy(c1, str3);
+//    strcat(c1, str4);
+//
+//    char *cResult = (char *) malloc(strlen(c) + strlen(c1) + 1);
+//    strcpy(cResult, c);
+//    strcat(cResult, c1);
+//
+//    env->DeleteLocalRef(vcode);
+//    env->ReleaseStringUTFChars(token, str1);
+//    env->ReleaseStringUTFChars(auth_time, str2);
+//    env->ReleaseStringUTFChars(auth_version, str3);
 
     MD5 md5 = MD5(cResult);
     std::string md5Result = md5.hexdigest();
--- CPPDemo/app/src/main/java/com/house365/cppdemo/MainActivity.kt
+++ CPPDemo/app/src/main/java/com/house365/cppdemo/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.house365.cppdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.house365.cppdemo.databinding.ActivityMainBinding
-import com.house365.rent.RentAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +15,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Example of a call to a native method
-        val rentAuth = RentAuth()
-        binding.sampleText.text = rentAuth.stringFromJNI("2AI[a]m[a]cnV7mkiDc5N2W5qjJjYmY5ZTY0YjNmZTEzYjRjYjEwZWVkMjUxODA1NzViNGJkZWYxM2IyYWViY2JmYTEwMTRhNTU5ZjI2YzlmNWTGEzQ4PBOStVQq5yh27lbI0Vby7sr[a]qCJAZ1n0SEgbrmL87mlKcdGb/Hb[a]6BWpzpu7EpWO6pUw6JGkUFp5eYn6wkR0PoE8MeTVFkHnWoCIQqUhR9mM8PL8UDzWoUTA4Z8TyL8Att9FV1sIPFS3QT17",
-            "1624588333", "4.2.1")
+        binding.sampleText.setOnClickListener {
+            startActivity(Intent(this@MainActivity, Main2Activity::class.java))
+        }
     }
 }
